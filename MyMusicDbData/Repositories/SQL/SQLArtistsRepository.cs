8433fa1bc19724b78491849442937a47e5a68592
LorenzoVDAB <111412610+LorenzoVDAB@users.noreply.github.com>
2023-04-25 23:07:38 +0200

    + DataLayer Repositories, Interfaces and SQL Implementation + CRUD operations + ArtistsService

---
+++ MyMusicDbData/Repositories/SQL/SQLArtistsRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMusicDbData.Models;
+using MyMusicDbData.Repositories.Interfaces;
+using System.Numerics;
+
+namespace MyMusicDbData.Repositories.SQL;
+
+public class SQLArtistsRepository : IArtistsRepository {
+    private readonly MyMusicDbContext context; 
+
+    public SQLArtistsRepository(MyMusicDbContext context) {
+        this.context = context; 
+    }
+
+    public async Task<Artist> AddAsync(Artist artist) {
+        context.Artists.Add(artist);
+        await SaveAsync();
+        return artist; 
+    }
+
+    public async Task DeleteAsync(int id) {
+        Artist? artist = await context.Artists.FindAsync(id);
+        if (artist != null) {
+            context.Artists.Remove(artist);
+            await SaveAsync();
+        }
+    }
+
+    //Using pagination in case the database gets big 
+    public async Task<IEnumerable<Artist>> GetAllAsync(int page, int pageSize) {
+        return await context.Artists
+            .OrderBy(a => a.Id)
+            .Skip(page * pageSize)
+            .Take(pageSize)
+            .ToListAsync(); 
+    }
+
+    public async Task<Artist?> GetByIdAsync(int id) {
+        return await context.Artists.FindAsync(id); 
+    }
+
+    public async Task SaveAsync() {
+        await context.SaveChangesAsync(); 
+    }
+
+    public async Task<Artist> UpdateAsync(Artist artist) {
+        try {
+            context.Artists.Update(artist);
+            await context.SaveChangesAsync();
+            return artist;
+        } catch (DbUpdateException ex) {
+            throw new Exception("Something went wrong while trying to update the database. ", ex);
+        }
+    }
+}
---
+++ MyMusicDbServices/ArtistsService.cs
@@ -0,0 +1,19 @@
+﻿using MyMusicDbData.Models;
+using MyMusicDbData.Repositories.SQL; 
+
+namespace MyMusicDbServices; 
+
+public class ArtistsService {
+    private readonly SQLArtistsRepository repository; 
+
+    public ArtistsService(SQLArtistsRepository repository) {
+        this.repository = repository;
+    }
+
+    public async Task<IEnumerable<Artist>> GetAllAsync(int page, int pageSize) => await repository.GetAllAsync(page, pageSize);
+    public async Task<Artist?> GetByIdAsync(int id) => await repository.GetByIdAsync(id);
+    public async Task<Artist> AddAsync(Artist artist) => await repository.AddAsync(artist);
+    public async Task<Artist> UpdateAsync(Artist artist) => await repository.UpdateAsync(artist);
+    public async Task DeleteAsync(int id) => await repository.DeleteAsync(id);
+    public async Task SaveAsync() => await repository.SaveAsync();
+}
